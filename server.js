c82ab89abbaa94d2bd353dac4b0e28feb2db397f
harwcam <84109744+harwcam@users.noreply.github.com>
2022-04-20 22:22:03 -0400

    Added ability to query by ID

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const { animals } = require('./data/animals.json')
 
 const PORT = process.env.PORT || 3001
-
 const app = express();
 
 
@@ -32,7 +31,11 @@ function filterByQuery(query, animalsArray) {
     }
     return filteredResults;
   }
-  
+
+function findById(id, animalsArray) {
+  const result = animalsArray.filter(animal => animal.id === id)[0];
+  return result
+}
 
 
 app.get('/api/animals', (req, res) => {
@@ -42,6 +45,16 @@ app.get('/api/animals', (req, res) => {
     }
     res.json(results);
 });
+
+app.get('/api/animals/:id', (req, res) => {
+  const result = findById(req.params.id, animals);
+  if (result) {
+    res.json(result);
+  } else {
+    res.send(404);
+  }
+});
+
 
 app.listen(PORT, () => {
     console.log(`API Server now on port ${PORT}!`);
